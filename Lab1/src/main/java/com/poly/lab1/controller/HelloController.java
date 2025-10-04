4e0080d74a408adfc90921e08a8bf9a78c06daae
khanhdcmts00903-svg <khanhdcmts00903@fpt.edu.vn>
2025-10-04 20:48:47 +0700

    TS00903 - Dang Cong Minh Khanh - Lab1

---
+++ Lab1/src/main/java/com/poly/lab1/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.poly.lab1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public 	class HelloController {
+	@RequestMapping("/poly/hello")
+	public String sayHello(Model model) {
+		model.addAttribute("title","FPT Polytechninc");
+		model.addAttribute("subject","Hello");
+		return "poly/hello";
+	}
+}
